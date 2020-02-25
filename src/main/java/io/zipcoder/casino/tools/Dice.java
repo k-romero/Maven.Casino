841c550669d64a8824f4fc7806d2ed9cc133bf01
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-25 15:36:09 -0500

    dice object and corresponding classes complete

--- src/main/java/io/zipcoder/casino/tools/Dice.java
+++ src/main/java/io/zipcoder/casino/tools/Dice.java
@@ -1,4 +1,54 @@
 package io.zipcoder.casino.tools;
 
+import java.util.Random;
+
 public class Dice {
+
+    Integer numberOfDice;
+
+    public Dice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+
+    public Integer getNumberOfDice() {
+        return numberOfDice;
+    }
+
+    public void setNumberOfDice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+
+    public Integer rollDice() {
+        Random roll = new Random();
+        return roll.nextInt(6) + 1;
+    }
+
+
+    public Integer tossAndSum() {
+
+        Integer sum = 0;
+
+        for (int i = 1; i <= numberOfDice; i++){
+            sum += rollDice();
+        }
+
+        return sum;
+
+    }
+
+    public Integer tossAndSumWithSeed(Integer seed) {
+
+        Random roll = new Random(seed);
+        Integer sum = 0;
+
+        for (int i = 1; i <= numberOfDice; i++){
+            sum += roll.nextInt(6) + 1;
+        }
+
+        return sum;
+
+    }
+
+
+
 }
