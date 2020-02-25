2dc164447305bf006c3f754dd13d59fee1cee30b
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-02-25 14:38:15 -0500

    complete location enum

--- src/main/java/io/zipcoder/casino/tools/Location.java
+++ src/main/java/io/zipcoder/casino/tools/Location.java
@@ -1,4 +1,30 @@
 package io.zipcoder.casino.tools;
 
 public enum Location {
+    FOUR    ("4", 4),
+    FIVE    ("5",5),
+    SIX     ("6",6),
+    EIGHT   ("8",7),
+    NINE    ("9",9),
+    TEN     ("10",10),
+    PASS    ("pass",1),
+    NOTTPASS("notPass",-1),
+    COME    ("come",13),
+    NOTCOME ("notCome",-13);
+
+    private final String locationName;
+    private final int locationNumber;
+
+    public String getLocationString() {
+        return locationName;
+    }
+
+    public int getLocationNumber() {
+        return locationNumber;
+    }
+
+    Location(String locationName, int locationNumber){
+        this.locationName = locationName;
+        this.locationNumber = locationNumber;
+    }
 }
