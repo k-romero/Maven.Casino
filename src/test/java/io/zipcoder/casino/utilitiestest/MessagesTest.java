701f055658580d8f1b1d39a3b99af760be6ca4b4
k-romero <imkevinromero@gmail.com>
2020-02-25 13:27:51 -0500

    ensure package and file structure works

--- src/test/java/io/zipcoder/casino/utilitiestest/MessagesTest.java
+++ src/test/java/io/zipcoder/casino/utilitiestest/MessagesTest.java
@@ -1,4 +1,17 @@
 package io.zipcoder.casino.utilitiestest;
 
+import io.zipcoder.casino.utilities.Messages;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MessagesTest {
+
+    //Making sure file and package structure works correctly
+    @Test
+    public void testTest(){
+        Messages newclass = new Messages();
+
+        int actual = newclass.test();
+        Assert.assertEquals(1,actual);
+    }
 }
