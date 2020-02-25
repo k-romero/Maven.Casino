8aeecc23e5ad6b8bf0f4539c1684357a4108f122
k-romero <imkevinromero@gmail.com>
2020-02-25 14:46:33 -0500

    create player class and corresponding tests

--- src/main/java/io/zipcoder/casino/player/Player.java
+++ src/main/java/io/zipcoder/casino/player/Player.java
@@ -1,4 +1,48 @@
 package io.zipcoder.casino.player;
 
 public class Player {
+
+    int id;
+    String name;
+    int playerFunds;
+    boolean isDrunk;
+
+    public Player(int id, String name, int playerFunds, boolean isDrunk) {
+        this.id = id;
+        this.name = name;
+        this.playerFunds = playerFunds;
+        this.isDrunk = isDrunk;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayerFunds() {
+        return playerFunds;
+    }
+
+    public void setPlayerFunds(int playerFunds) {
+        this.playerFunds = playerFunds;
+    }
+
+    public boolean isDrunk() {
+        return isDrunk;
+    }
+
+    public void setDrunk(boolean drunk) {
+        isDrunk = drunk;
+    }
 }
--- src/test/java/io/zipcoder/casino/playertest/PlayerTest.java
+++ src/test/java/io/zipcoder/casino/playertest/PlayerTest.java
@@ -1,4 +1,145 @@
 package io.zipcoder.casino.playertest;
 
+import io.zipcoder.casino.player.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PlayerTest {
+
+    @Test
+    public void constructorTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        Assert.assertTrue(playerTest instanceof Player);
+    }
+
+    @Test
+    public void getIdTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        int expected = 1000;
+        int acutal = playerTest.getId();
+
+        Assert.assertEquals(expected,acutal);
+    }
+
+    @Test
+    public void setIdTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        playerTest.setId(1230);
+        int expected = 1230;
+        int actual = playerTest.getId();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getNameTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        String expected = "Player Test";
+        String actual = playerTest.getName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void setNameTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        playerTest.setName("Team J");
+        String expected = "Team J";
+        String actual = playerTest.getName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getPlayerFundsTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        int expected = 200;
+        int actual = playerTest.getPlayerFunds();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void setPlayerFundsTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        playerTest.setPlayerFunds(4500);
+        int expected = 4500;
+        int actual = playerTest.getPlayerFunds();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+
+
+    @Test
+    public void checkDrunkTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+
+        Assert.assertTrue(playerTest.isDrunk());
+    }
+
+    @Test
+    public void setDrunkTest(){
+        int id = 1000;
+        String name = "Player Test";
+        int playerFunds = 200;
+        boolean isDrunk = true;
+
+        Player playerTest = new Player(id,name,playerFunds,isDrunk);
+
+        playerTest.setDrunk(false);
+
+        Assert.assertFalse(playerTest.isDrunk());
+    }
+
+
+
 }
