57d376ba26976f3e2c47824065f4f3fc9e7e78cc
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-02-25 15:21:17 -0500

    created game abstract and gametest

--- src/main/java/io/zipcoder/casino/game/Game.java
+++ src/main/java/io/zipcoder/casino/game/Game.java
@@ -1,4 +1,22 @@
 package io.zipcoder.casino.game;
 
-public class Game {
+import io.zipcoder.casino.player.Player;
+
+public abstract class Game  {
+    Player player;
+    Player house;
+    int playBet;
+
+
+    public int compare(int sum1, int sum2){
+        if(sum1 > sum2){
+            return 1;
+        }
+        else if (sum1 < sum2){
+            return -1;
+        }
+        else
+            return 0;
+    }
+
 }
