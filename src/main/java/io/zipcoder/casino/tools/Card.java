ee6b3700c10feca4005543a466049699a6d256aa
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-02-25 15:24:01 -0500

    filled Game Class

--- src/main/java/io/zipcoder/casino/tools/Card.java
+++ src/main/java/io/zipcoder/casino/tools/Card.java
@@ -1,4 +1,15 @@
 package io.zipcoder.casino.tools;
 
 public class Card {
+    private Face face;
+    private Suit suit;
+
+    public Face getFace(){
+        return face;
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
 }
+
