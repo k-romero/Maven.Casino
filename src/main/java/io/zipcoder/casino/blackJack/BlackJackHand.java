5e8ef85e909205d2d5363c47a6ff60ca3862e36f
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-26 14:09:39 -0500

    add method for summing hand

--- src/main/java/io/zipcoder/casino/blackJack/BlackJackHand.java
+++ src/main/java/io/zipcoder/casino/blackJack/BlackJackHand.java
@@ -1,28 +1,38 @@
 package io.zipcoder.casino.blackJack;
 
 import io.zipcoder.casino.tools.Card;
-import io.zipcoder.casino.tools.Deck;
+import io.zipcoder.casino.tools.Face;
 import io.zipcoder.casino.tools.Hand;
+
 
 import java.util.ArrayList;
 
 public class BlackJackHand extends Hand {
 
+    public ArrayList<Card> cards;
 
-
-
-
-   /* public void dealFirstHand(){
-
+    public BlackJackHand() {
+        this.cards = new ArrayList<>();
     }
 
-    public void displayHand(){
 
+    public Integer getSumOfHand(boolean aceIs11){
+        int sum = 0;
+        for(Card c: cards){
+            if(c.getFace().getValue() > 10){
+                sum += 10;
+            } else if(c.getFace() == Face.ACE && aceIs11) {
+                sum += 11;
+            }
+            else {
+                sum += c.getFace().getValue();
+            }
+        }
+
+        return sum;
     }
 
-    public Integer getSumUpHand(ArrayList<> currentHand){
-        return 0;
-    }
-*/
+
+
 
 }
--- src/test/java/io/zipcoder/casino/blackjacktest/BlackJackHandTest.java
+++ src/test/java/io/zipcoder/casino/blackjacktest/BlackJackHandTest.java
@@ -1,4 +1,42 @@
 package io.zipcoder.casino.blackjacktest;
 
+import io.zipcoder.casino.blackJack.BlackJackHand;
+import io.zipcoder.casino.blackJack.BlackJackPlayer;
+import io.zipcoder.casino.tools.Card;
+import io.zipcoder.casino.tools.Face;
+import io.zipcoder.casino.tools.Hand;
+import io.zipcoder.casino.tools.Suit;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class BlackJackHandTest {
+
+    @Test
+    public void TestConstructor(){
+        // Given
+        //BlackJackHand hand = new BlackJackHand();
+        // When
+
+
+        // Then
+    }
+
+    @Test
+    public void TestGetSumOfHand(){
+        // Given
+        BlackJackHand hand = new BlackJackHand();
+
+        // When
+        hand.addCardToHand(new Card(Suit.CLUB, Face.FIVE ));
+        hand.addCardToHand(new Card(Suit.CLUB, Face.JACK ));
+        Integer expected = 15;
+        Integer actual = hand.getSumOfHand(false);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
