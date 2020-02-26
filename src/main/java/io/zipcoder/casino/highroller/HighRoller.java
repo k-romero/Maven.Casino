0ed25d5cd4d15ac7a44dfaad2ee2b23e8c9209ce
cwilliams <coreywllms9@gmail.com>
2020-02-26 14:36:56 -0500

    made changes to highroller_class

--- src/main/java/io/zipcoder/casino/highroller/HighRoller.java
+++ src/main/java/io/zipcoder/casino/highroller/HighRoller.java
@@ -1,4 +1,30 @@
 package io.zipcoder.casino.highroller;
 
-public class HighRoller {
+import io.zipcoder.casino.game.GamblingGame;
+import io.zipcoder.casino.game.GameControl;
+import io.zipcoder.casino.player.Player;
+
+public class HighRoller implements GameControl, GamblingGame {
+
+    Integer player1Bet = 0;
+
+
+    public void start(Player p1) {
+
+    }
+
+    public void end(Player p1) {
+
+    }
+
+    public void placeBet(Player p1, int amount) {
+        player1Bet = amount;
+
+
+    }
+
+    public void payout(Player p1, int amount) {
+
+    }
+
 }
