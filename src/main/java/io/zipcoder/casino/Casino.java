42dc1090449a815c5512e9aad884f7860b0d71a2
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-02-27 16:21:01 -0500

    user messages

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -1,9 +1,16 @@
 package io.zipcoder.casino;
 
 
+import io.zipcoder.casino.gofish.GoFish;
+import io.zipcoder.casino.player.Player;
+
 public class Casino {
     public static void main(String[] args) {
         // write your tests before you start fucking with this
         // GO TEAM
+
+        Player p1 = new Player(10, "JJ", 1000, true) ;
+        GoFish g = new GoFish();
+        g.start(p1);
     }
 }
--- src/main/java/io/zipcoder/casino/gofish/GoFishPlayer.java
+++ src/main/java/io/zipcoder/casino/gofish/GoFishPlayer.java
@@ -3,14 +3,17 @@ import io.zipcoder.casino.player.GamblingPlayer;
 import io.zipcoder.casino.player.Player;
 import io.zipcoder.casino.tools.Deck;
 import io.zipcoder.casino.tools.Face;
+import io.zipcoder.casino.utilities.Console;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 import static io.zipcoder.casino.game.CardGame.deal;
 
 public class GoFishPlayer implements GamblingPlayer {
     private GoFishHand myHand;
     private Player player;
+    Console console = new Console(System.in, System.out);
 
     public GoFishPlayer(Player player){
         this.myHand = new GoFishHand();
@@ -21,8 +24,9 @@ public class GoFishPlayer implements GamblingPlayer {
         return myHand;
     }
 
-    public Boolean askFor(GoFishPlayer they, Face f, Deck deck){
+    public Boolean askFor(GoFishPlayer they, Face f){
         if(they.getGoFishHand().doesMyHandHave(f)){
+            console.print(they.getPlayerData().getName()+" has "+ they.getGoFishHand().howManyDoIHave(f)+" "+f.getFaceString()+".");
             they.getGoFishHand().giveCardsTo(f, myHand);
             return true;
         }
@@ -49,23 +53,59 @@ public class GoFishPlayer implements GamblingPlayer {
         return 0;
     }
 
-
-    public GoFishPlayer promptForPlayer(ArrayList<GoFishPlayer> players){
-        //
-        //
-        //@TODO
-        //
-        //
-        return new GoFishPlayer(new Player(0,"",0,false));
+    public void showUserTheHand(){
+        console.print("Cards on your hand are: ");
+        this.getGoFishHand().displayHands();
     }
 
+    public GoFishPlayer promptForPlayer(ArrayList<GoFishPlayer> players){
+        int index;
+        while(true) {
+            console.println("[ Please select a player to ask ]");
+            for(int i=1; i<players.size(); i++){
+                console.println( " > ("+ i +") " + players.get(i).getPlayerData().getName() );
+            }
+            try {
+                index = console.getIntegerInput("I will select: ");
+                if(index>players.size()){
+                    throw new InputMismatchException();
+                }
+                break;
+            }catch (Exception e){
+                console.println("Invalid Input! Please try again.");
+            }
+        }
+        return players.get(index);
+    }
+
+//    public Face stringToFace(String input){
+//        for(Face f: Face.values()){
+//            if(f.getFaceString().equalsIgnoreCase(input))
+//                return f;
+//        }
+//        return null;
+//    }
+
     public Face promptForFace(){
-        //this.getGoFishHand().listEveryFaceIHave();
-        //
-        //@TODO
-        //print all the faces(let user choose)
-        //get back a face and return
-        return this.getGoFishHand().getCardsOnHand().get(0).getFace();
+        int val;
+        ArrayList<Face> fs;
+        while(true) {
+            console.println("[ Please select a face to ask for ]");
+            fs = this.getGoFishHand().listEveryFaceIHave();
+            for(int i=0; i<fs.size(); i++){
+                console.println( " > ("+ i +") "+ fs.get(i).getFaceString() );
+            }
+            try {
+                val = console.getIntegerInput("I will select: ");
+                if(val>fs.size()){
+                    throw new InputMismatchException();
+                }
+                break;
+            }catch (Exception e){
+                console.println("Invalid Input! Please try again.");
+            }
+        }
+        return fs.get(val);
     }
 
 
