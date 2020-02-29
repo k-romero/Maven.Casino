16aa546d14fc6a2ad9a9999d623ff5ad0e308498
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-02-28 23:45:56 -0500

    add some ANSI code, design and variation not finished yet

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -39,18 +39,29 @@ public class Casino {
         selection.put(Menu.BLACKJACK, ()-> startBlackJackSession(p));
     }
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     public void printLogo(){
         String logo =
-                "            == Welcome to the ==\n" +
+                ANSI_CYAN + "            == Welcome to the ==\n" + ANSI_PURPLE +
                 " ██████╗ █████╗ ███████╗██╗███╗   ██╗ ██████╗ \n" +
                 "██╔════╝██╔══██╗██╔════╝██║████╗  ██║██╔═══██╗\n" +
                 "██║     ███████║███████╗██║██╔██╗ ██║██║   ██║\n" +
                 "██║     ██╔══██║╚════██║██║██║╚██╗██║██║   ██║\n" +
                 "╚██████╗██║  ██║███████║██║██║ ╚████║╚██████╔╝\n" +
-                " ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ";
+                " ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ "+ ANSI_RESET ;
+
         c.println("");
-        c.println(logo);
+        c.printlnS(logo);
+
     }
 
     public void printMainMenu(){
